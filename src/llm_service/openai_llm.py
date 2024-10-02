fdc291f3b5bc8c3b35f15ac3103338aa45c9b4b5
chris-guerra <christianguerra94@gmail.com>
2024-10-02 13:01:41 -0300

    added Ollama static LLM

--- src/llm_service/openai_llm.py
+++ src/llm_service/openai_llm.py
@@ -1,53 +1,17 @@
 """
 LLM Script Oriented to using OpenAI structure
 """
+import os
+from dotenv import load_dotenv
 
-from langchain_core.prompts.few_shot import FewShotPromptTemplate
-from langchain_core.prompts.prompt import PromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_ollama import OllamaLLM
 
-# Initialize LLM
-MODEL = 'gpt-4o-mini'
-llm = ChatOpenAI(model_name= MODEL, temperature= 0)
+# Load environment variables
+load_dotenv()
+MODEL = os.getenv("LLAMA_MODEL")
 
-examples = [
-    {"color": "red", "emotion": "passion"},
-    {"color": "blue", "emotion": "serenity"},
-    {"color": "green", "emotion": "tranquility"},
-]
+# Load Model
+llm = OllamaLLM(model= MODEL)
 
-example_formatter_template = """
-Color: {color}
-Emotion: {emotion}
-"""
-
-example_prompt = PromptTemplate(
-    input_variables=["color", "emotion"],
-    template=example_formatter_template,
-)
-
-few_shot_prompt = FewShotPromptTemplate(
-    examples=examples,
-    example_prompt=example_prompt,
-    prefix="""Here are some examples of colors and the emotions associated with \
-them:""",
-    suffix="""Now, given a new color, identify the emotion associated with \
-it:\nColor: {input}\nEmotion:""",
-    input_variables=["input"],
-    example_separator="\n",
-)
-
-formatted_prompt = few_shot_prompt.format(input="purple")
-prompt=PromptTemplate(template=formatted_prompt, input_variables=[])
-
-# Create the LLMChain for the prompt
-chain = prompt | llm
-
-# Run the LLMChain to get the AI-generated emotion associated with the input
-# color
-response = chain.invoke({})
-
-# We print the example of the prompt sent
-print("Input: 'Color: purple'")
-print("Output: 'Emotion:", f"{response.content}'")
-print(f"\nPrompt:\n\n{prompt.template}")
+response = llm.invoke("The first man on the moon was ...")
+print(response)
